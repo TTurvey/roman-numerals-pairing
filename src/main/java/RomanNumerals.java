5516484576de904586a511a0088492dc7f2cd9f1
MattW <mattnwarnock@yahoo.co.uk>
2022-09-06 11:59:01 +0100

    With broken enum

--- src/main/java/RomanNumerals.java
+++ src/main/java/RomanNumerals.java
@@ -1,7 +1,44 @@
+import java.sql.SQLOutput;
+
 public class RomanNumerals {
+    enum Numerals {
+        FOUR("IV", 4),
+        FIVE("V", 5),
+        NINE("IX", 9),
+        TEN("X", 10),
+        FORTY("XL", 40),
+        FIFTY("L", 50),
+        NINETY("XC", 90),
+        HUNDRED("C", 100),
+        FOUR_HUNDRED("CD", 400),
+        FIVE_HUNDRED("D", 500),
+        NINE_HUNDRED("CM", 900),
+        THOUSAND("M", 1000);
+
+        public final String roman;
+        public final int number;
+
+        Numerals(String roman, int number) {
+            this.roman = roman;
+            this.number = number;
+        }
+    }
 
 
-    public String convert(int i) {
-        return "I";
+    public String convert(int input) {
+        String result = "";
+
+        for (Numerals numeral : Numerals.values()) {
+            while (input >= numeral.number) {
+                result += numeral.roman;
+                input -= numeral.number;
+                System.out.println(numeral.number);
+                System.out.println(numeral.roman);
+            }
+        }
+
+        result += "I".repeat(input);
+
+        return result;
     }
 }
--- src/test/java/RomanNumeralsTest.java
+++ src/test/java/RomanNumeralsTest.java
@@ -1,14 +1,41 @@
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RomanNumeralsTest {
+    RomanNumerals romanNumerals;
+    @BeforeEach
+    void setup() {
+        romanNumerals = new RomanNumerals();
+    }
 
-    @Test public
-    void return_I_when_one () {
-        assertEquals( "I" , new RomanNumerals().convert(1) );
+    @ParameterizedTest
+    @CsvSource({
+            "1, I",
+            "2, II",
+            "3, III",
+            "4, IV",
+            "5, V",
+            "6, VI",
+            "9, IX",
+            "10, X",
+            "20, XX",
+            "17, XVII",
+            "40, XL",
+            "60, LX",
+            "90, XC",
+            "100, C",
+            "400, CD",
+            "500, D",
+            "900, CM",
+            "1000, M"
+    })
+
+    void converts_number_to_numeral (int input, String output) {
+        assertEquals( output , romanNumerals.convert(input) );
     }
-    @Test public
-    void return_II_when_two () {
-        assertEquals( "II" , new RomanNumerals().convert(2) );
-    }
+
 }
